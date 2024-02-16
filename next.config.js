7985855649d5628493cf19c476503fae0ef7a4a0
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2024-02-16 14:54:02 -0300

    fix: fix the basePath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 // Create basePath for GitHub pages
-const basePath = process.env.NODE_ENV !== 'development' ? '/nextjs-mantine-bundle-analyzer' : '';
+const basePath = process.env.NODE_ENV !== 'development' ? '/nextjs-ant-design-bundle-analyzer' : '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
