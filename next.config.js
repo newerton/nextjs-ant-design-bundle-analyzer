eb622cd898c0ac6d8535f5b5050614f00ffc3b93
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2025-03-13 17:23:05 -0300

    chore: update Next.js configuration and streamline GitHub Actions workflow

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: true,
+  openAnalyzer: false,  
+});
+
+// Create basePath for GitHub pages
+const basePath = process.env.NODE_ENV !== 'development' ? '/nextjs-ant-design-bundle-analyzer' : '';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath,
+  experimental: {
+    reactCompiler: true,
+  },
+}
+
+module.exports = withBundleAnalyzer(nextConfig)
